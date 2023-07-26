b70b843b7a942b7acdb59c26c4c295ebd1433b0e
paras nayal <parasnayal955@gmail.com>
2023-07-26 14:24:17 +0530

    rotate an array k steps rigth using juggling algorithm

--- Array/Medium/RotateAnArray.cpp
+++ Array/Medium/RotateAnArray.cpp
@@ -8,7 +8,8 @@
 using namespace std;
 
 void rotate(vector<int> &nums, int k);
-void reverse(vector<int> &nums, int start, int end);
+int findGCD(int num1, int num2);
+// void reverse(vector<int> &nums, int start, int end);
 void display(vector<int> &nums);
 
 int main(void)
@@ -56,17 +57,45 @@ int main(void)
 // Time Complexity => O(N)
 // Space Complexity => O(1)
 
-void reverse(vector<int> &nums, int start, int end)
+// void reverse(vector<int> &nums, int start, int end)
+// {
+
+//     while (start <= end)
+//     {
+//         int temp = nums[start];
+//         nums[start] = nums[end];
+//         nums[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
+
+// void rotate(vector<int> &nums, int k)
+// {
+//     int n = nums.size();
+//     k = k % n;
+
+//     if (n == 1 || k == 0)
+//     {
+//         return;
+//     }
+//     reverse(nums, n - k, n - 1);
+//     reverse(nums, 0, n - k - 1);
+//     reverse(nums, 0, n - 1);
+// }
+
+// Using juggling algorithm
+// Time complexity => O(N)
+// Space Complexity => O(1)
+
+int findGCD(int num1, int num2)
 {
 
-    while (start <= end)
+    if (num1 == 0)
     {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        start++;
-        end--;
+        return num2;
     }
+    return findGCD(num2 % num1, num1);
 }
 
 void rotate(vector<int> &nums, int k)
@@ -78,9 +107,27 @@ void rotate(vector<int> &nums, int k)
     {
         return;
     }
-    reverse(nums, n - k, n - 1);
-    reverse(nums, 0, n - k - 1);
-    reverse(nums, 0, n - 1);
+
+    int g_c_d = findGCD(k, n);
+
+    for (int i = 0; i < g_c_d; i++)
+    {
+
+        int temp = nums[n - k + i];
+        int j = n - k + i;
+
+        while (true)
+        {
+            int m = (j - k + n) % n;
+            if (m == n - k + i)
+            {
+                break;
+            }
+            nums[j] = nums[m];
+            j = m;
+        }
+        nums[j] = temp;
+    }
 }
 
 void display(vector<int> &nums)
