346cd706b69bf944021bc68e43fc059f8211f41c
paras nayal <parasnayal955@gmail.com>
2023-07-26 06:59:59 +0530

    rotate an array by using reversal algorithm

--- Array/Medium/RotateAnArray.cpp
+++ Array/Medium/RotateAnArray.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 void rotate(vector<int> &nums, int k);
+void reverse(vector<int> &nums, int start, int end);
 void display(vector<int> &nums);
 
 int main(void)
@@ -24,31 +25,62 @@ int main(void)
 // Brute force approach => using temp array
 // Time Complexity => O(n)
 // Space Complexity => O(K)
+// void rotate(vector<int> &nums, int k)
+// {
+//     int n = nums.size();
+//     k = k % n;
+//     if (n == 1 || k == 0)
+//     {
+//         return;
+//     }
+
+//     vector<int> temp(k);
+
+//     for (int i = 0; i < k; i++)
+//     {
+//         temp[i] = nums[n - 1 - i];
+//     }
+
+//     for (int i = n - 1; i >= k; i--)
+//     {
+//         nums[i] = nums[i - k];
+//     }
+
+//     for (int i = 0; i < k; i++)
+//     {
+//         nums[i] = temp[k - 1 - i];
+//     }
+// }
+
+// Optimal solution => using reversal algorithm
+// Time Complexity => O(N)
+// Space Complexity => O(1)
+
+void reverse(vector<int> &nums, int start, int end)
+{
+
+    while (start <= end)
+    {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
 void rotate(vector<int> &nums, int k)
 {
     int n = nums.size();
     k = k % n;
+
     if (n == 1 || k == 0)
     {
         return;
     }
-
-    vector<int> temp(k);
-
-    for (int i = 0; i < k; i++)
-    {
-        temp[i] = nums[n - 1 - i];
-    }
-
-    for (int i = n - 1; i >= k; i--)
-    {
-        nums[i] = nums[i - k];
-    }
-
-    for (int i = 0; i < k; i++)
-    {
-        nums[i] = temp[k - 1 - i];
-    }
+    reverse(nums, n - k, n - 1);
+    reverse(nums, 0, n - k - 1);
+    reverse(nums, 0, n - 1);
 }
 
 void display(vector<int> &nums)
