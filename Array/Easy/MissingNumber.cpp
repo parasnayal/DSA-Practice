3d2eab74aa5340d493d39bdd19464e9d55311db1
paras nayal <parasnayal955@gmail.com>
2023-08-11 23:56:41 +0530

    find missing number

---
+++ Array/Easy/MissingNumber.cpp
@@ -0,0 +1,41 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Input : nums = [ 9, 6, 4, 2, 3, 5, 7, 0, 1 ]
+// Output : 8
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int missingNumber(vector<int> &nums);
+int main(void)
+{
+    vector<int> nums{3, 0, 1};
+    cout << missingNumber(nums) << endl;
+    return 0;
+}
+
+int missingNumber(vector<int> &nums)
+{
+    int n = nums.size();
+    int missingNumber;
+    for (int i = 0; i <= n; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (i == nums[j])
+            {
+                cout << "inside"<<endl;
+                flag == 1;
+                // break;
+            }
+        }
+
+        if (flag == 0)
+        {
+            cout << i << endl;
+            missingNumber = i;
+        }
+    }
+    return missingNumber;
+}
