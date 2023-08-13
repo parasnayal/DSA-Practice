ac787ecb530646edc9576c7a2cf4f680ac381c30
paras nayal <parasnayal955@gmail.com>
2023-08-13 22:26:56 +0530

    find the single number present in an array using brute force approach

---
+++ Array/Easy/SingleNumber.cpp
@@ -0,0 +1,42 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int singleNumber(vector<int> &nums);
+
+int main(void)
+{
+    vector<int> nums{4, 1, 2, 1, 2};
+    singleNumber(nums);
+    return 0;
+}
+
+// Brute force approach
+// Time Complexity => O(n^2)
+
+int singleNumber(vector<int> &nums)
+{
+    int n = nums.size();
+    int uniqueNumber;
+    for (int i = 0; i < n; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                flag++;
+            }
+        }
+        if (flag == 1)
+        {
+            uniqueNumber = nums[i];
+        }
+    }
+    return uniqueNumber;
+}
