4d104366935c02306a3599d4a3bba9e4bdad50b6
paras nayal <parasnayal955@gmail.com>
2023-08-15 21:58:07 +0530

    length of the longest subarray with sum k by brute force approach

---
+++ Array/Easy/LongestSubarray.cpp
@@ -0,0 +1,38 @@
+// Find the length of the longest subarray of 'a' whose sum is equal to 'k'.
+// Input : 'n' = 7 , 'k' = 3
+// 'A' = [1,2,3,1,1,1,1]
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int longestSubarrayWithSumK(vector<int> &nums, long long k);
+
+int main(void)
+{
+    vector<int> nums{1, 2, 3, 1, 1, 1, 1};
+    long long sum = 3;
+    longestSubarrayWithSumK(nums, sum);
+    return 0;
+}
+
+// Brute force approach
+// Time Complexity => O(n^2)
+int longestSubarrayWithSumK(vector<int> &nums, long long k)
+{
+    int n = nums.size();
+    int length = 0;
+    for (int i = 0; i < n; i++)
+    {
+        long long sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += nums[j];
+            if (sum == k)
+            {
+                length = max(length, j - i + 1);
+            }
+        }
+    }
+    return length;
+}
